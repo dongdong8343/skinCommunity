cdfaae052e9636a59ea28d9fe113b94d31eb805c
dongdong8343 <fkdlem8343@naver.com>
2025-03-20 01:43:18 +0900

    좋아요 엔티티 개발

---
+++ src/main/java/com/project/skin/domain/post/Likes.java
@@ -0,0 +1,24 @@
+package com.project.skin.domain.post;
+
+import com.project.skin.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Likes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long likesId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
