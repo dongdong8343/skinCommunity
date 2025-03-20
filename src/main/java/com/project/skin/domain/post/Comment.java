6e886b41e48b913b21c124a112b50344138e6fed
dongdong8343 <fkdlem8343@naver.com>
2025-03-20 20:38:32 +0900

    댓글 엔티티 수정

--- src/main/java/com/project/skin/domain/post/Comment.java
+++ src/main/java/com/project/skin/domain/post/Comment.java
@@ -7,7 +7,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,17 +30,46 @@ public class Comment extends BaseTimeEntity {
     @JoinColumn(name = "comment_id")
     private Comment parent;
 
-    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, orphanRemoval = true)
     private List<Comment> child = new ArrayList<>();
 
     @Column(nullable = false)
     private String content;
 
-    @Column(nullable = false)
+    @Column(nullable = false) // 앞에 놈 알아야 할 듯
     private Integer depth;
 
-    @Column(nullable = false)
+    @Column(nullable = false) // 앞에 놈 알아야 할 듯
     private Integer commentOrder;
 
-    private LocalDateTime deletedAt;
+    private Comment(User user, Post post, String content, Integer depth, Integer commentOrder) {
+        this.user = user;
+        this.post = post;
+        this.content = content;
+        this.depth = depth;
+        this.commentOrder = commentOrder;
+    }
+
+    private Comment(User user, Post post, Comment parent, String content, Integer depth, Integer commentOrder) {
+        this.user = user;
+        this.post = post;
+        this.parent = parent;
+        this.content = content;
+        this.depth = depth;
+        this.commentOrder = commentOrder;
+    }
+
+    // 댓글 생성
+    public static Comment ofComment(User user, Post post, String content, Integer depth, Integer commentOrder) {
+        return new Comment(user, post, content, depth, commentOrder);
+    }
+
+    // 대댓글 생성
+    public static Comment ofSubComment(User user, Post post, Comment parent, String content, Integer depth, Integer commentOrder) {
+        return new Comment(user, post, parent, content, depth, commentOrder);
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
 }
