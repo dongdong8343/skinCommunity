cb9ec182f82bfef8737ee97fdc1b24dde6410b12
dongdong8343 <fkdlem8343@naver.com>
2025-03-20 20:38:26 +0900

    카테고리 엔티티 수정

--- src/main/java/com/project/skin/domain/category/Category.java
+++ src/main/java/com/project/skin/domain/category/Category.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +21,7 @@ public class Category extends BaseTimeEntity {
     @JoinColumn(name = "category_id")
     private Category parent;
 
-    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, orphanRemoval = true)
     private List<Category> child = new ArrayList<>();
 
     @Column(length = 20, nullable = false)
@@ -29,6 +30,24 @@ public class Category extends BaseTimeEntity {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
-    private boolean is_active;
+    private Category(Category parent, String code, String name) {
+        this.parent = parent;
+        this.code = code;
+        this.name = name;
+    }
+
+    private Category(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    // 카테고리 생성
+    public Category ofCategory(String code, String name) {
+        return new Category(code, name);
+    }
+
+    // 서브 카테고리 생성
+    public Category ofSubCategory(Category parent, String code, String name) {
+        return new Category(parent, code, name);
+    }
 }
