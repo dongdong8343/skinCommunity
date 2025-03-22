3643a40d920e42a633c7f12467f8ac110c3fcddb
dongdong8343 <fkdlem8343@naver.com>
2025-03-22 17:46:53 +0900

    카테고리 엔티티 수정

--- src/main/java/com/project/skin/domain/category/Category.java
+++ src/main/java/com/project/skin/domain/category/Category.java
@@ -15,14 +15,14 @@ import java.util.List;
 public class Category extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long categoryId;
+    private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category parent;
 
-    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, orphanRemoval = true)
-    private List<Category> child = new ArrayList<>();
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY)
+    private List<Category> children = new ArrayList<>();
 
     @Column(length = 20, nullable = false)
     private String code;
@@ -30,24 +30,23 @@ public class Category extends BaseTimeEntity {
     @Column(nullable = false)
     private String name;
 
-    private Category(Category parent, String code, String name) {
-        this.parent = parent;
-        this.code = code;
-        this.name = name;
-    }
-
     private Category(String code, String name) {
         this.code = code;
         this.name = name;
     }
 
+    private Category(Category parent, String code, String name) {
+        this(code, name);
+        this.parent = parent;
+    }
+
     // 카테고리 생성
-    public Category ofCategory(String code, String name) {
+    public Category createCategory(String code, String name) {
         return new Category(code, name);
     }
 
     // 서브 카테고리 생성
-    public Category ofSubCategory(Category parent, String code, String name) {
+    public Category createSubCategory(Category parent, String code, String name) {
         return new Category(parent, code, name);
     }
 }
