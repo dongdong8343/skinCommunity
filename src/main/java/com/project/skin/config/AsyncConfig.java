ee08a755cdad88c6368eae14515f0b38c6e0e94b
dongdong8343 <fkdlem8343@naver.com>
2025-04-22 23:00:26 +0900

    설정 따로 설정 파일로 뺐음 - 리팩토링

---
+++ src/main/java/com/project/skin/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.project.skin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+}
