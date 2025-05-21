f930ab928a04f3dfc6c68ad90819bdbb9df26101
dongdong8343 <fkdlem8343@naver.com>
2025-05-21 21:41:23 +0900

    🚚 Rename: UserNotFoundException 이름 수정

--- src/main/java/com/project/skin/global/error/exception/UserNotFoundException.java
+++ src/main/java/com/project/skin/global/error/exception/UserNotFoundException.java
@@ -3,11 +3,11 @@ package com.project.skin.global.error.exception;
 import com.project.skin.global.error.ErrorCode;
 
 public class UserNotFoundException extends BusinessBaseException {
-    public UserNotFoundException(ErrorCode errorCode) {
-        super(errorCode);
-    }
+	public UserNotFoundException(ErrorCode errorCode) {
+		super(errorCode);
+	}
 
-    public UserNotFoundException() {
-        super(ErrorCode.USER_NOT_FOUND);
-    }
+	public UserNotFoundException() {
+		super(ErrorCode.USER_NOT_FOUND);
+	}
 }
